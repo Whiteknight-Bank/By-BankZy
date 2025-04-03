7c6478fa75bfdf8383aba8d4bf40480521bb8cd2
Whiteknight-Bank <icuxxgodfowarxx@gmail.com>
2025-04-03 23:09:52 +0700

    Update Test001.lua

--- Test001.lua
+++ Test001.lua
@@ -5699,6 +5699,44 @@ if self.Name == 'Drown' and _G.nodmgwater then
     end
 end)
 
+page5:Toggle("Anti Dark Pool", false,function(anstun)
+    _G.antistun = anstun
+end)
+
+
+spawn(function()
+    while wait() do
+        if _G.antistun then
+            pcall(function()
+local ResourceHolder = game.Workspace:FindFirstChild("ResourceHolder")
+
+    if ResourceHolder then
+        for _, player in pairs(game.Players:GetPlayers()) do
+            local resourceFolderName = "Resources_" .. tostring(player.UserId)
+            local playerResourceFolder = ResourceHolder:FindFirstChild(resourceFolderName)
+
+            if playerResourceFolder then
+                local magmaPool = playerResourceFolder:FindFirstChild("MagmaPool")
+
+                if magmaPool then
+                    for _, item in pairs(magmaPool:GetDescendants()) do
+                        if item:IsA("Instance") and item.Name == "TouchInterest" then
+                            item:Destroy()
+                            print("Removed TouchInterest from MagmaPool in", playerResourceFolder.Name)
+                        end
+                    end
+                else
+                    warn("MagmaPool not found in " .. resourceFolderName)
+                end
+            else
+                warn("Resources folder not found for player: " .. resourceFolderName)
+            end
+        end
+    else
+        warn("ResourceHolder not found in Workspace!")
+    end
+end
+
 page5:Label(" ┇ Unbox ┇ ")
 
 page5:Toggle("Unbox (Common)", false,function(ubcm)
