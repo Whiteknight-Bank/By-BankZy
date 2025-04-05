1ee0cce8b6fb4cd8efcc169d9beedebb4f49b928
Whiteknight-Bank <icuxxgodfowarxx@gmail.com>
2025-04-05 22:11:36 +0700

    Create EZ-Hub.lua

---
+++ EZ-Hub.lua
@@ -0,0 +1,16 @@
+--script
+local Window = create:Win("InW Hub : For Map OPL: Anarchy / No Update Now.")
+game:GetService("CoreGui").redui.MainSceen.Visible = false
+
+create:Notifile("", "   Welcome " .. game.Players.LocalPlayer.Name .. " To OP:L", 5)
+
+game:GetService("CoreGui").redui.MainSceen.Visible = true
+
+local Tap1 = Window:Taps("Autos")
+local page1 = Tap1:newpage()
+
+page1:Label(" ┇ Function Autos ┇ ")
+
+page1:Toggle("Auto Claim Mission", false,function(dmmsv)
+    AutoMission = dmmsv
+end)
