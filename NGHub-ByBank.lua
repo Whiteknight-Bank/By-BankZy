a7755701ed28d380a4eba39cf20fed775cc0faf2
Whiteknight-Bank <icuxxgodfowarxx@gmail.com>
2025-05-28 00:59:20 +0700

    Update NGHub-ByBank.lua

--- NGHub-ByBank.lua
+++ NGHub-ByBank.lua
@@ -1,11 +1,23 @@
 local create = loadstring(game:HttpGet("https://raw.githubusercontent.com/Whiteknight-Bank/By-BankZy/refs/heads/main/Libinw.lua"))()
-local Window = create:Win("Test Hub")
-create:Notifile("", "Welcome "..game.Players.LocalPlayer.Name, 5)
+local Window = create:Win("InW Hub : For Map OPL")
+
+create:Notifile("", "   Welcome " .. game.Players.LocalPlayer.Name .. " To OP:L", 5)
 
 local Tap1 = Window:Taps("Autos")
 local page1 = Tap1:newpage()
 
-page1:Label(" ┇ Function Autos ┇ ")
-page1:Toggle("Auto Claim Mission", false, function(v)
-    AutoMission = v
+page1:Section("ฟังก์ชันอัตโนมัติ")
+
+page1:Toggle("Auto Claim Mission", false, function(state)
+    AutoMission = state
+end)
+
+spawn(function()
+    while task.wait(1) do
+        if AutoMission then
+            pcall(function()
+                workspace.Merchants.ExpertiseMerchant.Clickable.Retum:FireServer()
+            end)
+        end
+    end
 end)
