6b078025b6ddac49822115bc9e5c3ada7ae2c47e
Whiteknight-Bank <icuxxgodfowarxx@gmail.com>
2025-05-28 11:19:17 +0700

    Update NGHub-ByBank.lua

--- NGHub-ByBank.lua
+++ NGHub-ByBank.lua
@@ -8,19 +8,17 @@ local Tab1 = Window:Taps("Autos")
 local page1 = Tab1:newpage()
 
 page1:Label("┇ Function Autos ┇")
-page1:Section("Mission Control")
-
 page1:Toggle("Auto Claim Mission", false, function(state)
     AutoMission = state
 end)
 
 -- เพิ่มอีกแท็บ
-local Tab2 = Window:Taps("Farming")
+local Tab2 = Window:Taps("Autos")
 local page2 = Tab2:newpage()
 
-page2:Section("Farming Area")
+page2:Label("┇ Function Farming ┇")
 page2:Toggle("Auto Farm", false, function(state)
-    AutoFarm = state
+    AutoMission = state
 end)
 
 local Tab3 = Window:Taps("Players")
@@ -32,9 +30,25 @@ page3:Dropdown("Select Player", {"Option 1", "Option 2", "Option 3"}, function(s
 end)
 
 page3:Button("Click to Tp", function()
-    create:Notifile("", "คุณกดปุ่มเรียบร้อยแล้ว!", 3)
+    create:Notifile("", "Clicked", 2)
 end)
 
 page3:Toggle("Behind", false, function(state)
     AutoFarm = state
+end)
+
+local Tab4 = Window:Taps("Island")
+local page4 = Tab4:newpage()
+
+page4:Label("┇ Function Teleport ┇")
+page4:Button("Click to Tp", false, function()
+    create:Notifile("", "Clicked!", 2)
+end)
+
+local Tab5 = Window:Taps("Misc")
+local page5 = Tab5:newpage()
+
+page5:Label("┇ Function Anti ┇")
+page5:Toggle("Anti Dmg Water", false, function(state)
+    AutoMission = state
 end)
