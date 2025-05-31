d1ca64a9fcb710cbfce56707486357eb60bcedcf
Whiteknight-Bank <icuxxgodfowarxx@gmail.com>
2025-05-31 21:04:20 +0700

    Update BankHub-OPL.lua

--- BankHub-OPL.lua
+++ BankHub-OPL.lua
@@ -10,3 +10,23 @@ local page2 = window:Taps("ตั้งค่า")
 page2:Button("รีโหลด", function() 
  print("รี") 
  end)
+
+local page3 = window:Taps("ระบบ")
+page3:Toggle("เปิดระบบ", function(v) 
+ print("ระบบ:", v) 
+ end)
+
+local page4 = window:Taps("ตั้งค่า")
+page4:Button("รีโหลด", function() 
+ print("รี") 
+ end)
+
+local page5 = window:Taps("ระบบ")
+page5:Toggle("เปิดระบบ", function(v) 
+ print("ระบบ:", v) 
+ end)
+
+local page6 = window:Taps("ตั้งค่า")
+page6:Button("รีโหลด", function() 
+ print("รี") 
+ end)
