a70221489f28f9d77666e3d8dfa76926ce79b95f
Whiteknight-Bank <icuxxgodfowarxx@gmail.com>
2025-05-31 21:22:30 +0700

    Update BankHub-OPL.lua

--- BankHub-OPL.lua
+++ BankHub-OPL.lua
@@ -2,9 +2,34 @@ local create = loadstring(game:HttpGet("https://raw.githubusercontent.com/Whitek
 local window = create:Window("Bank Hub")
 
 local page1 = window:Taps("Autos")
+page1:Toggle("Auto Claim Expertise", function(v) 
+ print("ระบบ:", v) 
+ end)
+
+page1:Toggle("Auto Bring Devil Fruit", function(v) 
+ print("ระบบ:", v) 
+ end)
+
+page1:Toggle("Auto Claim Challengs", function(v) 
+ print("ระบบ:", v) 
+ end)
+
+page1:Toggle("Auto Claim Gift Beri", function(v) 
+ print("ระบบ:", v) 
+ end)
+
+page1:Toggle("Auto Claim Gift Gems", function(v) 
+ print("ระบบ:", v) 
+ end)
+
+page1:Toggle("Auto Bring Chest", function(v) 
+ print("ระบบ:", v) 
+ end)
+
 page1:Toggle("เปิดระบบ", function(v) 
  print("ระบบ:", v) 
  end)
+
 
 local page2 = window:Taps("Farm")
 page2:Button("รีโหลด", function() 
