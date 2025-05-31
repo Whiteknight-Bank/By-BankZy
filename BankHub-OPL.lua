ac5fe68db8c4edbc17c30c032a9a528b8f2199fc
Whiteknight-Bank <icuxxgodfowarxx@gmail.com>
2025-05-31 18:05:06 +0700

    Update BankHub-OPL.lua

--- BankHub-OPL.lua
+++ BankHub-OPL.lua
@@ -1,11 +1,12 @@
 local create = loadstring(game:HttpGet("https://raw.githubusercontent.com/Whiteknight-Bank/By-BankZy/refs/heads/main/Ui_Lib.lua/Boltlib.lua"))()
 local window = create:Window("Bank Hub")
 
-local page = window:Taps("Main")
-page:Toggle("Auto Claim Expertise", function(state)
-    print("Toggle:", state)
+local page1 = window:Taps("ระบบ")
+page1:Toggle("เปิดระบบ", function(v) 
+print("ระบบ:", v) 
 end)
 
-page:Button("Button", function()
-    print("ปุ่มถูกกด")
+local page2 = window:Taps("ตั้งค่า")
+page2:Button("รีโหลด", function() 
+print("รี") 
 end)
