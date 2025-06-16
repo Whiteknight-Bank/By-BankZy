d1d15ce69fa8b2085fec4cff5c7a0ba336895365
Whiteknight-Bank <icuxxgodfowarxx@gmail.com>
2025-06-16 18:39:42 +0700

    Update InWHuh-Steve.lua

--- InWHuh-Steve.lua
+++ InWHuh-Steve.lua
@@ -444,7 +444,57 @@ if _G.farmAll then
         end)
     end)
 end	
+			end)
 
+spawn(function()
+while task.wait() do
+pcall(function()
+if _G.farmAll then
+local player = game.Players.LocalPlayer
+local char = player.Character or player.CharacterAdded:Wait()
+local tool = char:FindFirstChildOfClass("Tool")
+local offset = -10 -- default
+
+if tool then  
+                local toolName = tool.Name  
+
+                -- ตรวจสอบดาบ  
+                for _, v in pairs(Cache.DevConfig["ListOfSword"]) do  
+                    if string.find(v, toolName) then  
+                        offset = -8  
+                        break  
+                    end  
+                end  
+
+                -- ตรวจสอบ melee  
+                for _, v in pairs(Cache.DevConfig["ListOfMelee"]) do  
+                    if string.find(v, toolName) then  
+                        offset = -5  
+                        break  
+                    end  
+                end  
+            end  
+
+            -- ลากมอนทุกตัว  
+            for _, mob in pairs(workspace.Npcs:GetChildren()) do  
+                if mob:FindFirstChild("HumanoidRootPart") and mob:FindFirstChild("Humanoid") then  
+                    local root = mob.HumanoidRootPart  
+                    root.CanCollide = false  
+                    root.Size = Vector3.new(10, 10, 10)  
+                    root.Anchored = true  
+                    root.CFrame = char.HumanoidRootPart.CFrame * CFrame.new(0, 0, offset)  
+
+                    if mob.Humanoid.Health <= 0 then    
+                        root.Size = Vector3.new(0, 0, 0)    
+                        mob:Destroy()    
+                    end    
+                end  
+            end  
+        end  
+    end)  
+end
+end)
+		
 page1:Label("┇ Function Haki ┇")
 page1:Toggle("Auto Buso", false, function(hki)
     _G.autobuso = hki
