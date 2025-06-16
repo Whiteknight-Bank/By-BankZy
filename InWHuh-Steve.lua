5718bed759d8188ce998a9b60b7928ce5191acb8
Whiteknight-Bank <icuxxgodfowarxx@gmail.com>
2025-06-16 15:31:36 +0700

    Update InWHuh-Steve.lua

--- InWHuh-Steve.lua
+++ InWHuh-Steve.lua
@@ -1300,6 +1300,45 @@ page5:Toggle("Anti AFK", false, function(state)
     end
 end)
 
+local RunService = game:GetService("RunService")
+local followConnection
+local seaPart
+
+page5:Toggle("Walk On Sea", false, function(walk)
+    if walk then
+        create:Notifile("", "You can walk on sea now! :)", 3)
+
+        seaPart = Instance.new("Part")
+        seaPart.Name = "InvisibleSea"
+        seaPart.Anchored = true
+        seaPart.CanCollide = true
+        seaPart.Transparency = 1
+        seaPart.Size = Vector3.new(50, 1, 50)
+        seaPart.Parent = workspace
+
+        followConnection = RunService.RenderStepped:Connect(function()
+            local char = plr.Character or plr.CharacterAdded:Wait()
+            local root = char:FindFirstChild("HumanoidRootPart")
+            if root and seaPart then
+                local goalPos = Vector3.new(root.Position.X, -5.5, root.Position.Z)
+                seaPart.Position = seaPart.Position:Lerp(goalPos, 0.5)
+            end
+        end)
+
+    else
+        create:Notifile("", "Off walk on sea now! :(", 3)
+
+        if followConnection then
+            followConnection:Disconnect()
+            followConnection = nil
+        end
+        if seaPart then
+            seaPart:Destroy()
+            seaPart = nil
+        end
+    end
+end)
+
 page5:Toggle("Teleport All Chest", false, function(cpst)
     _G.tpchest = cpst
 end)
