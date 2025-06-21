d7656946ccb73d700f6ff6a153c14d94655233f3
Whiteknight-Bank <icuxxgodfowarxx@gmail.com>
2025-06-22 01:36:40 +0700

    Update Lol.lua

--- Lol.lua
+++ Lol.lua
@@ -316,11 +316,6 @@ spawn(function()
 end)
 
 page1:Label("┇ Function Farm ┇")
-    
-local function leftClick()
-        game:GetService("VirtualUser"):CaptureController() 
-        game:GetService("VirtualUser"):Button1Down(Vector2.new(1280, 672))
-		end
 
 local SelectedMob = ""
 
@@ -721,26 +716,27 @@ function startAutoFarm()
                 end
             end
 
-            if tool and (equippedToolName ~= tool.Name or equippedKills ~= kills) then
-                _G.forceHold = true
-                humanoid:EquipTool(tool)
-                equippedToolName = tool.Name
-                equippedKills = kills
-                wait(0.75)
-                if tool.Parent == character then
-                    wait(0.75)
-                    leftClick()
-                end
-                wait(0.5)
-                _G.forceHold = false
-            end
+if tool and (equippedToolName ~= tool.Name or equippedKills ~= kills) then
+    _G.forceHold = true
+    humanoid:EquipTool(tool)
+    equippedToolName = tool.Name
+    equippedKills = kills
+    wait(0.75)
+    if tool.Parent == character then
+        wait(0.75)
+        game:GetService("VirtualUser"):CaptureController()
+        game:GetService("VirtualUser"):Button1Down(Vector2.new(1280, 672))
+    end
+    wait(0.5)
+    _G.forceHold = false
+end
 
-            if humanoid.Health <= 0 then
-                humanoid:UnequipTools()
-                equippedToolName = nil
-                equippedKills = -1
-                _G.forceHold = false
-            end
+if humanoid.Health <= 0 then
+    humanoid:UnequipTools()
+    equippedToolName = nil
+    equippedKills = -1
+    _G.forceHold = false
+end
         end)
     end)
 end
