1ce6c9d9cda2b7f9b71fa9b26630da37558bf655
Whiteknight-Bank <icuxxgodfowarxx@gmail.com>
2025-09-07 12:19:58 +0700

    Update Mythical.lua

--- ReaperXHub/Mythical.lua
+++ ReaperXHub/Mythical.lua
@@ -4,7 +4,11 @@ local win = lib:Win("ReaperX Hub")
 lib:Notifile("Alert", "This is a test message!", 3)
 
 local tab1 = win:Taps("Main")
-local tab2 = win:Taps("Settings")
+tab1:Label("Status: Waiting...")
+
+tab1:Dropdown("Select Item", {"Apple", "Banana", "Orange"}, function(selected)
+    print("You picked:", selected)
+end)
 
 tab1:Button("Click Me", function()
     print("Button Clicked!")
@@ -14,12 +18,7 @@ tab1:Toggle("Enable Feature", false, function(state)
     print("Toggle is:", state)
 end)
 
-tab1:Dropdown("Select Item", {"Apple", "Banana", "Orange"}, function(selected)
-    print("You picked:", selected)
-end)
-
-tab1:Label("Status: Waiting...")
-
+local tab2 = win:Taps("Settings")
 local section = tab2:DropdownTab("Extra Settings")
 
 section:Button("Sub Button", function()
@@ -30,12 +29,5 @@ section:Toggle("Sub Toggle", true, function(val)
     print("Sub toggle:", val)
 end)
 
-local section = tab2:DropdownTab("Extra Settings")
 
-section:Button("Sub Button", function()
-    print("Sub button pressed!")
-end)
 
-section:Toggle("Sub Toggle", true, function(val)
-    print("Sub toggle:", val)
-end)
